433ca938e4a37408d5a19bf5ab5d44a5e28f035e
RonakPatel2468 <143102592+RonakPatel2468@users.noreply.github.com>
2024-10-08 03:14:42 +0530

    Re-strucutre

--- app.js
+++ app.js
@@ -7,50 +7,57 @@ const errorHandler = require('./middlewares/errorHandler');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
-const logger = require('./middlewares/logger');
 const rateLimit = require('express-rate-limit');
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-  });
-  
+const logger = require('./middlewares/logger');
 
 dotenv.config();
-connectDB
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/books', bookRoutes);
+// Connect to MongoDB
+connectDB();
 
-
-app.use(
-    helmet({
-      contentSecurityPolicy: false,
-      crossOriginEmbedderPolicy: false,
-    })
-  );
+const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors());
+
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+});
 app.use(limiter);
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/books', bookRoutes);
 
 // Logger middleware
 app.use((req, res, next) => {
     logger.info(`${req.method} ${req.url}`);
     next();
-  });
+});
 
 // Error Handling Middleware
 app.use(errorHandler);
 
 // Root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the E-Library Management API!');
-  });
+    res.send(`
+        <center>
+            <h1>Welcome to the E-Library Management API!</h1>
+            <br>
+            <p>
+                Get E-Library API: 
+            <a href="https://github.com/RonakPatel2468/E-LIBRARY_MANAGEMENT.git" target="_blank">Repository: E-LIBRARY_MANAGEMENT</a>
+            </p>
+        </center>
+    `);
+});
 
+
+// Start the server
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
