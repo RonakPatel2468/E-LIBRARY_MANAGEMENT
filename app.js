8e4b1300c00c54fe222ef099d03beed54e3d9d6b
RonakPatel2468 <143102592+RonakPatel2468@users.noreply.github.com>
2024-10-08 01:47:52 +0530

    Add an Root route

--- app.js
+++ app.js
@@ -38,20 +38,19 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(limiter);
 
-
-
 // Logger middleware
 app.use((req, res, next) => {
     logger.info(`${req.method} ${req.url}`);
     next();
   });
 
-  
-
-
-
 // Error Handling Middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the E-Library Management API!');
+  });
+
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
